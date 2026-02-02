5e3d3097def4769b208f57bd03317262b240d997
nagendranmahavigneshshree-commits <nagendranmahavigneshshree@gmail.com>
2026-02-02 19:00:46 +0530

    Create ProductMain.java

---
+++ ProductMain.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Product {
+    int pid;
+    double price;
+    int quantity;
+
+    Product(int pid, double price, int quantity) {
+        this.pid = pid;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+public class ProductMain {
+
+    static double calculateTotal(Product[] products) {
+        double total = 0;
+        for (Product p : products) {
+            total += p.price * p.quantity;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Product[] products = new Product[5];
+
+        for (int i = 0; i < 5; i++) {
+            int pid = sc.nextInt();
+            double price = sc.nextDouble();
+            int quantity = sc.nextInt();
+            products[i] = new Product(pid, price, quantity);
+        }
+
+        int maxPid = products[0].pid;
+        double maxPrice = products[0].price;
+
+        for (int i = 1; i < 5; i++) {
+            if (products[i].price > maxPrice) {
+                maxPrice = products[i].price;
+                maxPid = products[i].pid;
+            }
+        }
+
+        System.out.println("Product ID with highest price: " + maxPid);
+        System.out.println("Total amount spent: " + calculateTotal(products));
+    }
+}
